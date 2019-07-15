4d2fdb7b74d7df6d71d957f48fee24e2a54c0b08
Andriy Yednarovych <andsei91@gmail.com>
2019-07-15 13:46:19 +0200

    Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,38 @@
 <template>
-    <div id="app">
-        <!-- <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">FreeFolk</a>
-        <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
       >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link">
-                        <router-link to="/">Youtube</router-link>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link">
-                        <router-link to="/converter">Converter</router-link>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link">
-                        <router-link to="/torrent">Torrent</router-link>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <div class="container">
-        <keep-alive>
-            <router-view />
-        </keep-alive>
-        </div>-->
-        <div class="ff-bar">
-            <div class="ff-logo">
-                <img src="./assets/logo.png" alt="">
-            </div>
-        </div>
-        <keep-alive>
-            <router-view/>
-        </keep-alive>
-    </div>
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-.ff-logo {
-    width: 30px;
-    height: 30px;
-    margin-left: 5px;
-    display: flex;
-    align-items: center;
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
+  }
 }
-.ff-logo img {
-    height: 20px;
-}
-.ff-bar {
-    background-color: #212121;
-    max-height: 30px;
-    display: flex;
-    align-items: center;
-    -webkit-app-region: drag;
-}
-#nav {
-    margin-bottom: 20px;
-}
-#app {
-    font-family: "Roboto", sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
-body {
-    background-color: #303030;
-    color: white;
-}
-</style>
+</script>
